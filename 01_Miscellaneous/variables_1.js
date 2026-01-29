709b1fe6ca18283207c46ea4df1f239527299a61
Nilket <joni0010@stud.kea.dk>
2026-01-29 10:33:33 +0100

    Tilføjede noget

---
+++ 01_Miscellaneous/variables_1.js
@@ -0,0 +1,29 @@
+
+//Node REPL
+
+//Type coercion
+//Strict equality
+
+
+//Rule 1: Always use const, unless change is needed then use let.
+
+//Const is constant in the assignment
+const luckyNumber = 4;
+
+console.log(luckyNumber);
+
+
+//JS Datatyper
+//String, Boolean, Number, BigInt, Object, Symbol, null, undefined. 
+
+const person = {
+    //Key-value pair
+    name: "Flemming"
+};
+
+
+const people = [];
+
+people.push("Victor");
+
+console.log(people);
